bff93b6a14be7597eade5e6f2f61f5f3fed80370
bteodora <149783651+bteodora@users.noreply.github.com>
2024-05-26 19:12:22 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const app = express()
 
 const userRoute = require('./routes/userRoute')
 const factoryRoute = require('./routes/factoryRoute')
+//const chocolateRoute = require('./routes/chocolateRoute')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -11,6 +12,7 @@ app.use(cors())
 
 app.use('/api/user', userRoute)
 app.use('/api/factory', factoryRoute)
+//app.use('/api/chocolate', chocolateRoute)
 
 const PORT = 5000
 app.listen(PORT, () => {
