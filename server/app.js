892a685e83335ef67de3c3a40525f23b9e356d2b
bteodora <149783651+bteodora@users.noreply.github.com>
2024-05-28 18:34:16 +0200

    removed comments

--- server/app.js
+++ server/app.js
@@ -4,7 +4,8 @@ const app = express()
 
 const userRoute = require('./routes/userRoute')
 const factoryRoute = require('./routes/factoryRoute')
-//const chocolateRoute = require('./routes/chocolateRoute')
+// server\routes\chocolateRoute.js
+const chocolateRoute = require('./routes/chocolateRoute')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -12,7 +13,7 @@ app.use(cors())
 
 app.use('/api/user', userRoute)
 app.use('/api/factory', factoryRoute)
-//app.use('/api/chocolate', chocolateRoute)
+app.use('/api/chocolate', chocolateRoute)
 
 const PORT = 5000
 app.listen(PORT, () => {
